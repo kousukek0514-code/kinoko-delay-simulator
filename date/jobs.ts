7f9806118b4b53671f61307bb53c395af481ee20
kousukek0514-code <kousuke.k.0514@gmail.com>
2026-07-11 23:53:41 +0900

    feat: 職業データ追加

---
+++ date/jobs.ts
@@ -0,0 +1,15 @@
+export type Job = {
+  id: string;
+  name: string;
+};
+
+export const jobs: Job[] = [
+  {
+    id: "yashin",
+    name: "矢神",
+  },
+  {
+    id: "haou",
+    name: "羽王",
+  },
+];
